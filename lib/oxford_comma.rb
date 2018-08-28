ca48212d64f4d7e7ed049649bc68cd258b161d7c
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 03:26:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     return array[0]
   elsif array.size == 2
     array.join " and "
-  elsif array.size == 3
-    array[0..1].join(", ")
+  elsif array.size >= 3
+    array[0...-1].join(", ")
     
   end 
 end 
