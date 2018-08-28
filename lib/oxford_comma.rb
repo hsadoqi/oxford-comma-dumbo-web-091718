be623d8abbe6ce8af46e8f870a6ffc13580b699b
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-28 03:32:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join " and "
   elsif array.size >= 3
-    array.last.insert(0, "and")
+    array.last.insert(0, "and ")
     array[0..-1].join(", ")
   end 
 end 
